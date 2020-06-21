518ba21fc6a42bf8c45801bc5615212098995474
GodDoesPlayDice <alik_okroyan@mail.ru>
2020-06-21 15:53:21 +0300

    trying to add react native vector icons to xcode

--- MobileApp/screens/home.tsx
+++ MobileApp/screens/home.tsx
@@ -6,12 +6,15 @@ import {
   Button,
 } from 'react-native';
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 // Home screen declaration
 const HomeScreen = (props: any) => {
   return (
     <View style={styles.root}>
       <Text>Строительная компания Регион строй</Text>
+      {myIcon}
       <Button
         title='Новости компании'
         color='#710ce3'
